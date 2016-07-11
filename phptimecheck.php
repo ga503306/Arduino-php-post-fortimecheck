b1e930b56ca4962142571c89cb909f050a9e5cae
hzt <ga203306@yahoo.com.tw>
2016-07-11 21:13:16 +0800

    Update phptimecheck.php

--- phptimecheck.php
+++ phptimecheck.php
@@ -3,31 +3,25 @@ mysql_connect('localhost','root','');
 mysql_select_db("userregisterclassroom");
 $sendthis = $_POST["sendthis"];
 
-$sqldata = "SELECT data FROM `registerclassroom` WHERE userid = " . $_POST["sendthis"];
-$sqldataout = "SELECT dataout FROM `registerclassroom` WHERE userid = " . $_POST["sendthis"];
+$sqldate = "SELECT date FROM `registerclassroom` WHERE userid = " . $_POST["sendthis"];
+$sqldataout = "SELECT dateout FROM `registerclassroom` WHERE userid = " . $_POST["sendthis"];
 
-
-$data = mysql_query($sqldata);
-$dataout= mysql_query($sqldataout);
-
-$resultdata = mysql_fetch_row($data);
-$resultdataout = mysql_fetch_row($dataout);
-
-echo "$resultdata[0]";
-echo "$resultdataout[0]";
-$data_time =  strtotime($resultdata[0]);
-$dataout_time =  strtotime($resultdataout[0]);
-
-
+$date = mysql_query($sqldate);
+$dateout= mysql_query($sqldateout);
+$resultdate = mysql_fetch_row($date);
+$resultdateout = mysql_fetch_row($dateout);
+echo "$resultdate[0]";
+echo "$resultdateout[0]";
+$date_time =  strtotime($resultdate[0]);
+$dateout_time =  strtotime($resultdateout[0]);
 echo "<br>";
-echo $data_time;
+echo $date_time;
 echo "<br>";
-echo $dataout_time;
+echo $dateout_time;
 echo "<br>";
 $nowtime = time();
 echo $nowtime;
-
-if($nowtime-$data_time>0 && $nowtime-$dataout_time<0)
+if($nowtime-$date_time>0 && $nowtime-$dateout_time<0)
 	echo "yes";
 else
 	echo "no";
